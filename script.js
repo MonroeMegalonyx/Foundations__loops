9e0a3df4c9f3042df4e6c5a511e75c83cdc42b86
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-01-12 11:22:31 -0500

    loop exercises

---
+++ script.js
@@ -0,0 +1,43 @@
+// LIGHTNING EXERCISE
+// Psychotic Bird
+// In your JavaScript file, write a while loop that outputs the phrase "TWEET TWEET" 100 times to the console.
+
+var i = 0;
+
+while (i < 100){
+    console.log("TWEET TWEET");
+    i++;
+  }
+
+// LIGHTNING EXERCISE
+// Lightning Exercise
+// Psychotic Bird: For Loop Edition
+// In your JavaScript, write a for loop that outputs the phrase "TWEET TWEET TWEET" 100 times to the console.
+
+for(var i = 0; i < 100; i++){
+    console.log("TWEET TWEET TWEET");
+  }
+
+// Favorite Foods
+// In your JavaScript, create a new variable called favoriteFoods. It should store an array of strings representing five of your favorite foods.
+// Loop through the array and print each food item to the console.
+
+let favoriteFoods = ["burgers", "fries", "pasta", "chicken", "pizza"]
+
+for(var i = 0; i < favoriteFoods.length; i++){
+    console.log(favoriteFoods[i]);
+  }
+
+// PRACTICE
+
+// 1. Student Names
+// 2. Interests
+// 3. Favorite Interest
+// 4. Harry Potter Titles
+// 5. Student Grades
+// 6. Two, Four, Six, Eight - Who do we appreciate?! CHEER
+// 7. Student Grades Take Two
+// 8. Interrupting Cow
+// 9. Student Grades
+
+// Challenges
